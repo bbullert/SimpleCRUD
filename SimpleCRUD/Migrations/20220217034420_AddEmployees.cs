78a53850c10a0ffc23f3b72e4f431ff1e4459882
bullert <b.bullert5@gmail.com>
2022-02-17 04:49:05 +0100

    Create migration which adds employees

---
+++ SimpleCRUD/Migrations/20220217034420_AddEmployees.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleCRUD.Migrations
+{
+    public partial class AddEmployees : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(35)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(35)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Email = table.Column<string>(type: "varchar(60)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "varchar(11)", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(10,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
