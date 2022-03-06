f2a31e6af735a5064e6d65be194b40fc39f15d9c
bullert <b.bullert5@gmail.com>
2022-03-06 13:12:56 +0100

    Add employee view model

---
+++ SimpleCRUD/Models/EmployeeViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCRUD.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(35, ErrorMessage = "{0} must be at most {1} characters long")]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(35, ErrorMessage = "{0} must be at most {1} characters long")]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth date")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [MaxLength(60, ErrorMessage = "{0} must be at most {1} characters long")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        // 11 digit long phone nubmer only
+        [Required(ErrorMessage = "{0} is required")]
+        [Remote("VerifyPhoneNumber", "Employee")]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Remote("VerifySalary", "Employee")]
+        [DataType(DataType.Currency)]
+        public string Salary { get; set; }
+    }
+}
